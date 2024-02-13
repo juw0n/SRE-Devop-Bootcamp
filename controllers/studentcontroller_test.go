da3337b4b370084e9e9ee19a6fb18980e2bd0063
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2024-02-13 18:54:32 +0100

    mocking DB

--- controllers/studentcontroller_test.go
+++ controllers/studentcontroller_test.go
@@ -2,17 +2,59 @@ package controllers
 
 import (
 	"context"
+	"net/http"
+	"testing"
 
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
 	db "github.com/juw0n/SRE-Devop-Bootcamp/database/sqlc"
-	"github.com/stretchr/testify/mock"
+	dbMocks "github.com/juw0n/SRE-Devop-Bootcamp/mocks"
+	// "go.uber.org/mock/gomock"
 )
 
-// Database interface
-type DBInterface interface {
-	CreateStudent(ctx context.Context, args db.CreateStudentParams) (db.Student, error)
-}
+func TestCreateStudent(t *testing.T) {
+	// Create a new instance of the mock controller
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-// MockDB is a mock implementation of the database interface for testing
-type MockDB struct {
-	mock.Mock
+	// Create a mock for the db.Queries interface
+	mockDB := dbMocks.NewMockQuerier(ctrl)
+
+	// Create a context
+	ctx := context.TODO()
+
+	// Create a new instance of the student controller with the mock
+	stController := NewStudentController(mockDB, ctx)
+
+	// Generate first and last names first
+	// firstName := util.RandomFirstName()
+	// lastName := util.RandomLastName()
+
+	// // Prepare sample input payload
+	// payload := &reqvalidate.CreateStudent{
+	// 	FirstName:    firstName,
+	// 	LastName:     lastName,
+	// 	Gender:       util.RandomGender(),
+	// 	DateOfBirth:  util.RandomDateOfBirth(2000, 2099),
+	// 	PhoneNumber:  util.RandomPhoneNumber(),
+	// 	Email:        util.RandomEmail(firstName, lastName),
+	// 	YearOfEnroll: int32(util.RandomYearOfEnrollment(2005, 2099)),
+	// 	Country:      util.RandomCountries(),
+	// 	Major:        util.RandomMajor(),
+	// }
+
+	// Mock the behavior of CreateStudent method in the mock
+	mockDB.EXPECT().CreateStudent(ctx, gomock.Any()).Return(&db.Student{}, nil)
+
+	// Create a mock gin context
+	ginCtx, _ := gin.CreateTestContext(nil)
+	ginCtx.Set("testKey", "testValue")
+
+	// Call the function under test
+	stController.CreateStudent(ginCtx)
+
+	// Check the response
+	if ginCtx.Writer.Status() != http.StatusCreated {
+		t.Errorf("Expected status %d but got %d", http.StatusCreated, ginCtx.Writer.Status())
+	}
 }
