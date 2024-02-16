ac453ab9a051509cd87140fbdb13a2587dfd9bea
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2024-02-16 19:19:37 +0100

    added readme

--- controllers/studentcontroller_test.go
+++ controllers/studentcontroller_test.go
@@ -26,23 +26,6 @@ func TestCreateStudent(t *testing.T) {
 	// Create a new instance of the student controller with the mock
 	stController := NewStudentController(mockDB, ctx)
 
-	// Generate first and last names first
-	// firstName := util.RandomFirstName()
-	// lastName := util.RandomLastName()
-
-	// // Prepare sample input payload
-	// payload := &reqvalidate.CreateStudent{
-	// 	FirstName:    firstName,
-	// 	LastName:     lastName,
-	// 	Gender:       util.RandomGender(),
-	// 	DateOfBirth:  util.RandomDateOfBirth(2000, 2099),
-	// 	PhoneNumber:  util.RandomPhoneNumber(),
-	// 	Email:        util.RandomEmail(firstName, lastName),
-	// 	YearOfEnroll: int32(util.RandomYearOfEnrollment(2005, 2099)),
-	// 	Country:      util.RandomCountries(),
-	// 	Major:        util.RandomMajor(),
-	// }
-
 	// Mock the behavior of CreateStudent method in the mock
 	mockDB.EXPECT().CreateStudent(ctx, gomock.Any()).Return(&db.Student{}, nil)
 
